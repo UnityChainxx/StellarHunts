104d3ece63ccb23dbe808e27066b1f8bedaecd9f
Yusuf Falade <faladeyusuf54@gmail.com>
2025-05-18 20:45:52 +0100

    Repo clean up and minor changes

--- backend/src/api-tracking/dto/update-api-tracking.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateApiTrackingDto } from './create-api-tracking.dto';
-
-export class UpdateApiTrackingDto extends PartialType(CreateApiTrackingDto) {}
--- backend/src/boosters/dto/claim-achievement.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsNotEmpty, IsUUID } from "class-validator"
-
-export class ClaimAchievementDto {
-  @IsUUID()
-  @IsNotEmpty()
-  achievementId: string
-}
-
