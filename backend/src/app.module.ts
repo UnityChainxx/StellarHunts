68620f5aa83d99c3b0fc0fccbac379a764c21a08
Ibinola <ibinolamichael1@gmail.com>
2025-07-01 18:53:22 +0100

    feat: implement api key module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -21,6 +21,7 @@ import { User } from "./auth/entities/user.entity"
 import { TimeTrial } from "./time-trial/time-trial.entity"
 import { Puzzle } from "./puzzle/puzzle.entity"
 import { Category } from "./puzzle-category/entities/category.entity"
+import { ApiKeyModule } from './api-key/api-key.module';
 
 @Module({
   imports: [
@@ -58,6 +59,7 @@ import { Category } from "./puzzle-category/entities/category.entity"
     TimeTrialModule,
     InAppNotificationsModule,
     NFTClaimModule,
+    ApiKeyModule,
   ],
   controllers: [AppController],
   providers: [AppService],
